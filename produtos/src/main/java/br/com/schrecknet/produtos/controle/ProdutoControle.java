11764d86a678b16f2cd1fb6f83ae94882d7c9516
bruno <bruno.ams@hotmail.com>
2023-05-20 19:17:01 -0300

    front

--- produtos/src/main/java/br/com/schrecknet/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/schrecknet/produtos/controle/ProdutoControle.java
@@ -2,6 +2,7 @@ package br.com.schrecknet.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.schrecknet.produtos.modelo.RespostaModelo;
 import br.com.schrecknet.produtos.servico.ProdutoServico;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProdutoControle {
 
     @Autowired
